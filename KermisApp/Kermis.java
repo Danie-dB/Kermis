a8c8661700e9d723a97879ac8ff76d8bffaeb6be
DaniedBatWS <d.debackker@workingspirit.nl>
2019-04-29 19:33:30 +0200

    Kermis opzet

---
+++ KermisApp/Kermis.java
@@ -0,0 +1,5 @@
+package KermisApp;
+
+public class Kermis {
+
+}
---
+++ KermisApp/KermisMain.java
@@ -0,0 +1,10 @@
+package KermisApp;
+
+public class KermisMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
